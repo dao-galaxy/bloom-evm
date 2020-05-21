d6f9f70d64aa1bb565345d6059bc23c8e2f8cb49
GameArtsDev <gameartsdev@protonmail.com>
2020-05-21 08:47:22 +0800

    modify

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,4 @@
 mod account_cmd;
-mod call_message_cmd;
-mod example;
 mod deposit_cmd;
 mod contract_cmd;
 
@@ -8,8 +6,6 @@ use std::collections::BTreeMap;
 
 use structopt::StructOpt;
 use account_cmd::AccountCmd;
-use call_message_cmd::CallMessageCmd;
-use example::CreateContractCmd;
 use deposit_cmd::DepositCmd;
 use contract_cmd::ContractCmd;
 
@@ -23,8 +19,6 @@ use evm::Config;
 #[derive(Debug, Clone, StructOpt)]
 pub enum Subcommand {
 	Account(AccountCmd),
-	Call(CallMessageCmd),
-	Create(CreateContractCmd),
 	Deposit(DepositCmd),
 	Contract(ContractCmd),
 }
@@ -55,12 +49,6 @@ impl Subcommand {
 		match self {
 			Subcommand::Account(cmd) => {
 				cmd.run(backend);
-			}
-			Subcommand::Call(cmd) => {
-				cmd.run(&mut executor);
-			}
-			Subcommand::Create(cmd) => {
-				cmd.run(&mut executor);
 			}
 			Subcommand::Deposit(cmd) => {
 				cmd.run(&mut executor);
