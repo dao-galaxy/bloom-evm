d74a54305b5dfd147cc82934622e660e9e9db426
jackoiu8722 <jackliu8722@gmail.com>
2020-08-12 12:25:13 +0800

    add client test

--- main/src/main.rs
+++ main/src/main.rs
@@ -39,3 +39,52 @@ pub fn run_server(end_point : &str,db: Arc<dyn (::kvdb::KeyValueDB)>, blockchain
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use zmq::{Context, DEALER, ROUTER};
+    use common_types::ipc::*;
+    use ethereum_types::Address;
+    use std::str::FromStr;
+    use rlp;
+
+    #[test]
+    fn account_info_test(){
+        let context = Context::new();
+        let socket = context.socket(DEALER).unwrap();
+        socket.set_identity( &hex!("bloom-evm").to_vec() ).unwrap();
+        socket.connect(END_POINT).unwrap();
+
+        let address = Address::from_str("26d1ec50b4e62c1d1a40d16e7cacc6a6580757d5").unwrap();
+        let req = AccountInfoReq(address);
+        let rlp_bytes = rlp::encode(&req);
+
+        let ipc_req = IpcRequest{
+            method:"AccountInfo".to_string(),
+            id: 1u64,
+            params: rlp_bytes,
+        };
+
+        let rlp_bytes = rlp::encode(&ipc_req);
+
+        socket.send(rlp_bytes,0).unwrap();
+        let mut received_parts = socket.recv_multipart(0).unwrap();
+        //println!("client thread, received from server, #received_parts: {:?}", received_parts);
+        let resp = received_parts.pop().unwrap();
+        println!(
+            "\tclient thread, received from server, #received_parts: {:x?};",
+            resp.clone()
+        );
+
+        let ipc_resp: IpcReply = rlp::decode(resp.as_slice()).unwrap();
+        let resp: AccountInfoResp = rlp::decode(ipc_resp.result.as_slice()).unwrap();
+        println!("address={:?}",address);
+        println!("----balance={:?}",resp.1);
+        println!("----nonce={:?}",resp.0);
+
+
+
+    }
+}
+
